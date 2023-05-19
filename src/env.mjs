ccdbeadb949558d1d973be3965acb0421c9423bd
tomimarkus991 <tomimarkusalber@gmail.com>
2023-05-19 16:32:50 +0300

    Add github ci and fix eslint errors

--- src/env.mjs
+++ src/env.mjs
@@ -38,6 +38,7 @@ const merged = server.merge(client);
 /** @typedef {z.infer<typeof merged>} MergedOutput */
 /** @typedef {z.SafeParseReturnType<MergedInput, MergedOutput>} MergedSafeParseReturn */
 
+// eslint-disable-next-line prefer-destructuring
 let env = /** @type {MergedOutput} */ (process.env);
 
 const skip =
@@ -54,10 +55,8 @@ if (!skip) {
   );
 
   if (parsed.success === false) {
-    console.error(
-      "❌ Invalid environment variables:",
-      parsed.error.flatten().fieldErrors,
-    );
+    // eslint-disable-next-line no-console
+    console.error("❌ Invalid environment variables:", parsed.error.flatten().fieldErrors);
     throw new Error("Invalid environment variables");
   }
 
@@ -70,7 +69,7 @@ if (!skip) {
         throw new Error(
           process.env.NODE_ENV === "production"
             ? "❌ Attempted to access a server-side environment variable on the client"
-            : `❌ Attempted to access server-side environment variable '${prop}' on the client`,
+            : `❌ Attempted to access server-side environment variable '${prop}' on the client`
         );
       return target[/** @type {keyof typeof target} */ (prop)];
     },
