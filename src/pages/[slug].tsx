089e624bb1410637c7357678e06f3d3a37075a7c
tomimarkus991 <tomimarkusalber@gmail.com>
2023-05-19 11:41:35 +0300

    Add new files for new routes

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from "next";
+
+const ProfilePage: NextPage = () => {
+  return (
+    <>
+      <main className="flex flex-col h-screen bg-slate-800">Profile</main>
+    </>
+  );
+};
+
+export default ProfilePage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 import { LoadingSpinner, RealButton } from "@/components";
 import { CreatePostForm } from "@/ui/";
@@ -18,24 +19,32 @@ const PostView = ({ author, post }: PostWithAuthor) => {
   return (
     <div className="bg-[#0C1222] text-white w-[30rem] p-4 rounded-2xl flex">
       {author.profileImageUrl ? (
-        <Image
-          alt="profile pic"
-          src={author.profileImageUrl}
-          className="mr-2 rounded-full"
-          width={56}
-          height={56}
-        />
+        <Link href={`@${author.username}`}>
+          <Image
+            alt="profile pic"
+            src={author.profileImageUrl}
+            className="mr-2 rounded-full"
+            width={56}
+            height={56}
+          />
+        </Link>
       ) : (
         <LoadingSpinner />
       )}
 
       <div className="flex flex-col">
         <div className="flex text-sm opacity-75">
-          <p className="mr-1">{`@${author.username}`}</p>
-          <p className="">{dayjs(post.createdAt).fromNow()}</p>
+          <Link href={`@${author.username}`}>
+            <p className="mr-1">{`@${author.username}`}</p>
+          </Link>
+          <Link href={`post/${post.id}`}>
+            <p className="">{dayjs(post.createdAt).fromNow()}</p>
+          </Link>
         </div>
 
-        <h1 className="text-2xl">{post.content}</h1>
+        <Link href={`post/${post.id}`}>
+          <h1 className="text-2xl">{post.content}</h1>
+        </Link>
       </div>
     </div>
   );
